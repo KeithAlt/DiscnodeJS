5823f4c177b4dd4d6c65defffd2bc56685037f4f
Keith <claymorebusinessinquiry@gmail.com>
2022-03-17 22:29:32 -0400

    Improved command perm. verification

--- src/commands.js
+++ src/commands.js
@@ -2,14 +2,9 @@ const config = require(`./config/config.json`);
 let discord;
 let client;
 
-// Permmision check by name
+// Permmision check by Discord role name
 function hasRole(member, requiredRole) {
   return member.roles.cache.some(role => role.name === requiredRole)
-}
-
-// Permmision check by admin priv
-function isAdmin(msg, member) {
-  return member.permissionsIn(msg.channelId).has("ADMINISTRATOR")
 }
 
 // Send a temporary message that will eventually explode
@@ -29,31 +24,32 @@ commands = {
   },
     ["state"] : {
         hasPerm : function(msg, member) {
-            return isAdmin(msg, member)
+            return member.isAdmin;
         },
         reqSyntax: `<command>`,
         reqArgLength: 0,
         func : function(msg, args) {
-          replyMsgTimed(`We have been fully operational for - **${Math.round(process.uptime()/60)} minutes**`, msg, 15);
+          replyMsgTimed(`We have been up for - **${Math.round(process.uptime()/60)} minutes**`, msg, 15);
         }
     },
 }
 
-// "Useful" globals
 module.exports = {
     initalize: function(discordObject, clientObject) {
         discord = discordObject;
         client = clientObject;
         console.log(`[${config.bot.name}] : Succesfully initalized commands`);
-
         return true;
     },
 
     handle: function(msg) {
         let args = msg.content.split(" ");
 
-        if (commands[args[0]] && commands[args[0]].hasPerm(msg, msg.member)) {
-            commands[args[0]].func(msg, args);
+        if (commands[args[0]]) {
+			msg.member.isAdmin = msg.member.permissionsIn(msg.channelId).has("ADMINISTRATOR");
+			if (msg.member.isAdmin || commands[args[0]].hasPerm(msg, msg.member)) {
+				commands[args[0]].func(msg, args);
+			}
         }
     }
 }
